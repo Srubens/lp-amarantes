79c48c96f3b0688744f8009679fc7585d16e477b
Rubens Filipe <rubensfilipe6@gmail.com>
2022-11-09 18:16:37 -0300

    add base64

--- src/pages/api/save.ts
+++ src/pages/api/save.ts
@@ -2,11 +2,16 @@ import type { NextApiRequest, NextApiResponse } from 'next'
 const { GoogleSpreadsheet } = require('google-spreadsheet')
 const doc = new GoogleSpreadsheet(process.env.SHEET_DOC_ID)
 
+const fromBase64 = (value:any) =>{
+    const buff = new Buffer(value,'base64')
+    return buff.toString('ascii')
+}
+
 const handler =  async(req:any, res:any) =>{
     try{
         await doc.useServiceAccountAuth({
             client_email:process.env.SHEET_CLIENT_EMAIL,
-            private_key: process.env.SHEET_PRIVATE_KEY
+            private_key: fromBase64(process.env.SHEET_PRIVATE_KEY)
         })
         await doc.loadInfo()
 
